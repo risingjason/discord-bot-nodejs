9724ffc1eec57cd3bf849999f4e0b09cd12ea24f
Jason Yatfai Zhang <jzhang5137@gmail.com>
2018-06-22 20:02:04 -0700

    added skeleton code for osrs and weather

---
+++ commands/osrs.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+const osrs = require('osrs-wrapper')
+
+module.exports.run = async (client, message, args) => {
+
+}
+
+module.exports.help = {
+  name: 'osrs',
+  descShort: 'Look up anyone on the Old School RuneScape highscores or look up any item on the OSRS Grand Exchange.'
+}
---
+++ commands/weather.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+const superagent = require('superagent')
+
+module.exports.run = async (client, message, args) => {
+
+}
+
+module.exports.help = {
+  name: 'weather',
+  descShort: 'Look up the weather based on location.'
+}
