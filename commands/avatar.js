df79c28f7db447721b7b31705857ac74739698fb
Jason Yatfai Zhang <jzhang5137@gmail.com>
2018-07-01 17:29:38 -0700

    fixed avatar bug

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,9 +4,12 @@ module.exports.run = async (client, message, args) => {
   // !avatar for self
   // !avatar @user for others
   if (args.length === 0) {
-    return message.channel.send(message.author + '\'s avatar is \n' + message.author.avatarURL)
+    return message.channel.send(message.author + '`\'s avatar is `\n' + message.author.avatarURL)
   } else {
     let firstUserMentioned = message.mentions.users.first()
+    if (!firstUserMentioned) {
+      return message.channel.send('`Please @ the person whose avatar you want to see.`')
+    }
 
     // need to add <@USER_ID> in order to have bot mention a different user
     return message.channel.send('<@' + firstUserMentioned.id + '>\'s avatar is \n' + firstUserMentioned.avatarURL)
@@ -15,6 +18,6 @@ module.exports.run = async (client, message, args) => {
 
 module.exports.help = {
   name: 'avatar',
-  parameters: '',
+  parameters: '<user>',
   descShort: 'Displays a larger image of your or another person\'s avatar.'
 }
